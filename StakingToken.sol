ccdb41d436062e7d99807d71473440207190f375
Salil Apte <salil.apte99@gmail.com>
2024-12-02 01:42:50 +0530

    Update StakingToken.sol

--- StakingToken.sol
+++ StakingToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakingToken is ERC20 {
+    constructor() ERC20("TestToken", "TTK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1,000,000 tokens to deployer's address
+    }
+}
 
